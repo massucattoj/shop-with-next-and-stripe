531f41c6cf926042eae2670861df9a11c029cd97
Jean Massucatto <massucattoj@Jeans-MacBook-Pro.local>
2024-08-19 09:58:38 -0300

    add basic structure and stripe prod

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
-import "@/styles/globals.css";
+import { globalStyles } from "@/styles/global";
 import type { AppProps } from "next/app";
 
+import logoImg from '../assets/logo.svg'
+import { Container, Header } from "@/styles/pages/app";
+import Image from "next/image";
+import { log } from "console";
+
+globalStyles()
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg.src} width={logoImg.width} height={logoImg.height} alt=""/>
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  )
 }
