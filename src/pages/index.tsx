3e1da6c497b4026d00e81a0b6eb4e901bf338e4e
Jean Massucatto <massucattoj@Jeans-MacBook-Pro.local>
2024-08-21 15:39:57 -0300

    add redirections and checkout session

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,13 +8,14 @@ import Stripe from "stripe"
 
 import { useKeenSlider } from "keen-slider/react"
 import 'keen-slider/keen-slider.min.css'
+import Head from "next/head"
 
 interface HomeProps {
   products: {
     id: string,
     name: string,
     imageUrl: string,
-    price: number
+    price: string
   }[]
 }
 
@@ -27,19 +28,30 @@ export default function Home({ products }: HomeProps) {
   })
 
   return (
-    <HomeContainer ref={sliderRef} className="keen-slider">
-      {products.map(product => {
-        return (
-          <Product key={product.id} className="keen-slider__slide">
-            <Image src={product.imageUrl} width={520} height={480} alt=""/>
-            <footer>
-              <strong>{product.name}</strong>
-              <span>{product.price}</span>
-            </footer>
-          </Product>
-        )
-      })}
-    </HomeContainer>
+    <>
+      <Head>
+        <title>Home | Ignite Shop</title>
+      </Head>
+
+      <HomeContainer ref={sliderRef} className="keen-slider">
+        {products.map(product => {
+          return (
+            <Product
+              href={`/product/${product.id}`}
+              key={product.id}
+              className="keen-slider__slide"
+              prefetch={false}
+            >
+              <Image src={product.imageUrl} width={520} height={480} alt=""/>
+              <footer>
+                <strong>{product.name}</strong>
+                <span>{product.price}</span>
+              </footer>
+            </Product>
+          )
+        })}
+      </HomeContainer>
+    </>
   )
 }
 
